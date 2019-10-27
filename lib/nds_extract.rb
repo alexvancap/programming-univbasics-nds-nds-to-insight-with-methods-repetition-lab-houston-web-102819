715f2140b18a91c67ffe2e985c351e928d620cef
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-27 00:37:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -35,7 +35,7 @@ def list_of_directors(source)
 end
 
 def total_gross(source)
-  pp directors_totals(directors_database)
+  pp directors_totals(directors_database)[0]
   pp list_of_directors(directors_database)
 
   # Write this implementation
